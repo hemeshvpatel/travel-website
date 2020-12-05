79fe8db1bf6db6949b5b6e0ac3d4e7ae10edf2c7
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-12-05 10:40:16 -0600

    fixed video by importing file like images

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,12 @@ import React from "react";
 import "../App.css";
 import { Button } from "./Button";
 import "./HeroSection.css";
+import video1 from "../videos/video-1.mp4";
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/src/videos/video-1.mp4" autoPlay loop muted />
+      <video src={video1} autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
